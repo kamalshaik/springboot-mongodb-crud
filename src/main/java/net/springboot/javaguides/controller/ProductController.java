38cd155c3c6c7479aa0ece38886e78527d645c87
Kamal <techlearn508@gmail.com>
2023-10-18 21:45:10 +0530

    first commit

---
+++ src/main/java/net/springboot/javaguides/controller/ProductController.java
@@ -0,0 +1,63 @@
+package net.springboot.javaguides.controller;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.springboot.javaguides.model.Product;
+import net.springboot.javaguides.service.ProductService;
+
+@RestController
+public class ProductController {
+
+	@Autowired
+	private ProductService productService;
+	
+	@GetMapping("/productTemplate")
+	public ResponseEntity<Product> getProductTemplate(){
+		Product product = new Product();
+		product.setDescription("testDEscription");
+		product.setId(1l);
+		product.setName("SKH");
+
+		return ResponseEntity.ok().body(product);
+	}
+	
+	@GetMapping("/products")
+	public ResponseEntity<List<Product>> getAllProduct(){
+		return ResponseEntity.ok().body(productService.getAllProduct());
+	}
+	
+	@GetMapping("/products/{id}")
+	public ResponseEntity<Product> getProductById(@PathVariable long id){
+		return ResponseEntity.ok().body(productService.getProductById(id));
+	}
+	
+	@PostMapping("/products")
+	public ResponseEntity<Product> createProduct(@RequestBody Product product){
+		return ResponseEntity.ok().body(this.productService.createProduct(product));
+	}
+	
+	@PutMapping("/products/{id}")
+	public ResponseEntity<Product> updateProduct(@PathVariable long id, @RequestBody Product product){
+		product.setId(id);
+		return ResponseEntity.ok().body(this.productService.updateProduct(product));
+	}
+
+	@DeleteMapping("/products/{id}")
+	public HttpStatus deleteProduct(@PathVariable long id){
+		this.productService.deleteProduct(id);
+		return HttpStatus.OK;
+	}
+}
